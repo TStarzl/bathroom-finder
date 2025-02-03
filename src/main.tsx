8ba48c820c603ed44bbf014c2a7684a56a146474
TStarzl <poppylilly348@gmail.com>
2025-02-02 23:17:13 -0600

    Unify project folder, fix Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './App.css'  // or './App.css' or whatever your file is named
+import './App.css' // or whichever file has @tailwind directives
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
